7b9534cc4e6424eac8fbe7d71378ec05a1ab1284
Svetlin Ralchev <iamralch@users.noreply.github.com>
2024-01-08 17:25:38 +0400

    Introduce the terraform-google-cloud-run
    Signed-off-by: Svetlin Ralchev <iamralch@users.noreply.github.com>

---
+++ versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "< 6.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "< 6.0"
+    }
+  }
+
+  provider_meta "google" {
+    module_name = "blueprints/terraform/terraform-google-cloud-run/v0.9.1"
+  }
+
+  provider_meta "google-beta" {
+    module_name = "blueprints/terraform/terraform-google-cloud-run/v0.9.1"
+  }
+}
